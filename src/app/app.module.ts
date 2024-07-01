b5a4283da9e68034d68b517b882bf732c5c13e53
Hy <hyhuynh222@gmail.com>
2024-07-01 12:28:44 -0700

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,27 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserService } from './user.service';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    UserService,
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
